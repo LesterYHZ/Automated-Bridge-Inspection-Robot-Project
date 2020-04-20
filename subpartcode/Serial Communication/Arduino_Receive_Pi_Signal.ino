31b086a885d33e49f25874160ba364a5f1532f1b
Lester Yang <lester842634640@gmail.com>
2020-04-20 00:42:45 -0400

    Updated .ino file

--- subpartcode/Serial Communication/Arduino_Receive_Pi_Signal.ino
+++ subpartcode/Serial Communication/Arduino_Receive_Pi_Signal.ino
@@ -10,5 +10,40 @@ void Receive_Signal(){
         incomingData = Serial.read();
         signal = int(incomingData);
         Serial.println(signal);
+
+        switch(signal){
+            case 0:
+            // Motor Stop
+                motor(0);
+                break;
+            case 1:
+            // Motor Forward
+                motor(1);
+                break;
+            case 2:
+            // Motor Backward
+                motor(2);
+                break;
+            case 3:
+            // Motor Right
+                motor(3);
+                break;
+            case 4: 
+            // Motor Left
+                motor(4);
+                break;
+            case 5:
+                pusher(1);
+                break;
+            case 6:
+                pusher(0);
+                break;
+            /*
+            * case other_number:
+            *   lcd("some_coord");
+            *   break;
+            */
+            
+        }
     }
 }
