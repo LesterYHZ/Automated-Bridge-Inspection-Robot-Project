97c04c726536e9e06b16c5b6300f98a6b7e86958
drea-11 <63472971+drea-11@users.noreply.github.com>
2020-04-27 12:47:14 -0400

    added in outline for code/FSM shell

--- finalcode/maincode_raspberrypi.py
+++ finalcode/maincode_raspberrypi.py
@@ -1,2 +1,35 @@
 #Main Code: Automated Bridge Inspection - Raspberry Pi
 #Group 1, ME588
+
+# import packages here ####################################
+
+
+# import different functions needed here ##########################
+
+
+#  FSM ##########################################################
+# inputs:
+# pan = camera servo1 angle readings (output from servo pid
+# tilt = camera servo2 angle readings
+# patch = pushing mechanism servo 3
+# ts = timer switch that begins 75 sec timer
+# rd = counter for the number of times entering row d
+# us1 = Ultrasonic sensor distance reading (pi pin)
+def fsm(self, pan, tilt, patch, ts, rd, us1, us2, us3, us4):
+
+    return (left_motor, right_motor, patching, state)
+
+  
+  
+
+
+# leave this section at bottom
+# Multiprocessing code that runs multiple processes ###################################################
+# link to help explain https://pythonprogramming.net/multiprocessing-python-intermediate-python-tutorial/
+if __name__ == '__main__':
+    for i in range(100):
+        p = multiprocessing.Process(target=fsm, args=())
+
+
+        p.start()
+        p.join()
